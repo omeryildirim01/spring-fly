56f02e03fb37999c53020cdb82c17355ff5de2aa
Ömer YILDIRIM <omer@i-luxus.de>
2020-05-09 16:55:20 +0300

    first commit

---
+++ src/main/java/com/yildirimomer/springfly/domain/model/Ticket.java
@@ -0,0 +1,52 @@
+package com.yildirimomer.springfly.domain.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Omer YILDIRIM on 9.05.2020.
+ * I-Luxus GmbH.
+ * omer@i-luxus.de
+ */
+
+@Entity
+@Table(name= "Ticket")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of={"id"}, callSuper = false)
+@ApiModel(value = "Ticket model", description = "description about Ticket model",parent = BaseEntity.class)
+public class Ticket extends BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @ApiModelProperty(name = "id",value = "id",dataType = "Long")
+    private Long id;
+
+    @ApiModelProperty(name = "ticketNumber",value = "ticketNumber",dataType = "String")
+    private String ticketNumber;
+
+    @ApiModelProperty(name = "gate",value = "gate",dataType = "String")
+    private String gate;
+
+    @ApiModelProperty(name = "ticketPrice",value = "ticketPrice",dataType = "Double")
+    private double ticketPrice;
+
+    @Basic
+    @Temporal(TemporalType.TIMESTAMP)
+    @ApiModelProperty(name = "checkinDate",value = "checkinDate",dataType = "Date")
+    private java.util.Date checkinDate;
+
+
+    @ApiModelProperty(name = "booking",value = "booking",dataType = "Booking")
+    @OneToOne(targetEntity =Booking.class ,cascade = CascadeType.ALL)
+    @JoinColumn(name = "booking_id", referencedColumnName = "id")
+    private Booking booking;
+
+}
---
+++ src/main/java/com/yildirimomer/springfly/tools/CardUtil.java
@@ -0,0 +1,9 @@
+package com.yildirimomer.springfly.tools;
+
+/**
+ * Created by Omer YILDIRIM on 9.05.2020.
+ * I-Luxus GmbH.
+ * omer@i-luxus.de
+ */
+public class CardUtil {
+}
